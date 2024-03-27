f9da099a0db7ffc3c9fe5eb062e874153f12421e
BurakABC <113483607+BurakABC@users.noreply.github.com>
2024-03-26 21:32:53 -0400

    Fixed Code for RandomNumbergenarator

--- TheCode/RandomNumber/Function.js
+++ TheCode/RandomNumber/Function.js
@@ -1,8 +1,16 @@
-let min;
-let max;
+function RandomNumber() {
+  // Retrieve the minimum and maximum values from the input elements
+  let min = parseInt(document.getElementById('min').value, 10);
+  let max = parseInt(document.getElementById('max').value, 10);
 
-function randomNumber(max, min) {
-        Math.random()
-      }
+  if(min > max) {
+    alert('Minimum should be less than maximum.');
+    return;
+  }
 
-document.getElementById("Random-Number").innerHTML + "";
+  // Generate a random number between min and max
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  // Display the random number in the div with id 'result'
+  document.getElementById('Random-Number').textContent = randomNumber;
+}
